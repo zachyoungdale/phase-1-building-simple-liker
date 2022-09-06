1ee647a293489e53e3acd65007736354d724d881
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-06 11:37:00 -0400

    commit tree 7d5ef69176f5ca52fb8a2dfbd458593191da60f9

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
+document
+  .getElementsByClassName("like-glyph")
+  .addEventListener("click", function (event) {
+    console.log(event);
+  });
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
